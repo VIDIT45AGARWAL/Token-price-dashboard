1ac7cc48db8011b2d592348bba0841f4ebe42999
VIDIT45AGARWAL <viditagarwal0123@gmail.com>
2025-09-27 20:10:44 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-blue-900 text-3xl font-bold'>
+          hello world
+      </div>
+    </>
+  )
+}
+
+export default App
